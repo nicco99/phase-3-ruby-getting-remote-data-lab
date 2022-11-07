9d31fceb31a2c9ddd687b1e92191294ab50e0d2d
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-07 23:06:49 +0300

    completd assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
 # Write your code here
+require "uri"
+require "net/http"
+require "json"
+class GetRequester
+attr_reader :url
+
+def initialize url 
+    @url = URI.parse(url)
+end
+
+
+def get_response_body
+response = Net::HTTP.get_response(self.url)
+response.body
+end
+def parse_json
+        JSON.parse(self.get_response_body)
+end
+
+end
